31e702ca027c156cec532c081b1524093f0d46f7
gera12 <ito.rojo@gmail.com>
2017-04-04 21:00:33 -0300

    cambio

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    protected $table = "products" 
+    protected $table = "product" 
     //explicitos en el nombre de la tabla a donde apunta el modelo
 
     protected $fillable = ['name', 'description' ,'subcategorie_id', 'price', 'photo_rute'];
